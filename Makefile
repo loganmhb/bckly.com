695dad14b50f3dfbbb40ecd359f0ad2e54ab7ad8
Logan Buckley <lbuckley@opinionlab.com>
2017-04-21 08:59:02 -0400

    Generate feed correctly

--- Makefile
+++ Makefile
@@ -3,29 +3,28 @@ PAGES := site/moby-dick-poems.html site/about.html site/asteroids/index.html
 POSTS_HTML := $(POST_SOURCES:posts/%.markdown=site/posts/%.html)
 PANDOC_FLAGS := --to html5 --smart --template template.html
 
-# TODO:
-# - Convert Jekyll templates to Pandoc
-# - Figure out what to do about non-post pages
-# - Generate an index page
-# - Generate RSS/atom feed
+site/index.html: $(POSTS_HTML) site/feed.xml site/main.css $(PAGES) gen.pl
+	./gen.pl index | pandoc $(PANDOC_FLAGS) -o $@
 
-site/index.html: $(POSTS_HTML) site/main.css $(PAGES) gen_index.pl
-	./gen_index.pl | pandoc $(PANDOC_FLAGS) -o $@
+site/feed.xml: $(POSTS_HTML) $(PAGES)
+	./gen.pl feed > $@
 
 site/main.css: main.css
 	cp main.css site/
 
 site/%.html: pages/%.markdown
+	@mkdir -p site
 	pandoc $(PANDOC_FLAGS) $< -o $@
 
 site/asteroids/index.html:
 	cp -r pages/asteroids site/asteroids
 
 site/posts/%.html: posts/%.markdown
+	@mkdir -p site/posts
 	pandoc $(PANDOC_FLAGS) $< -o $@
 
 clean:
-	rm -rf site/*.html
+	rm -rf site/*
 
 serve: site/index.html
-	cd site/ && python -m http.server
+	cd site/ && python3 -m http.server
---
+++ gen.pl
@@ -0,0 +1,102 @@
+#!/usr/bin/env perl
+#
+# Generates the index page and RSS feed for the site.
+#
+# Usage:
+#
+#    ./gen index
+#    ./gen feed
+
+use strict;
+use warnings;
+use POSIX qw(strftime);
+use Time::Piece;
+
+my $feed_template = '<?xml version="1.0" encoding="UTF-8"?>
+<rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
+  <channel>
+    <title>bckly.com</title>
+    <description>the blog of logan buckley</description>
+    <link>https://bckly.com/</link>
+    <atom:link href="https://bckly.com/feed.xml" rel="self" type="application/rss+xml"/>
+    <pubDate>NOW</pubDate>
+    <lastBuildDate>NOW</lastBuildDate>
+    POSTS
+  </channel>
+</rss>
+';
+
+my $item_template = '
+      <item>
+        <title>TITLE</title>
+        <description>DESCRIPTION</description>
+        <pubDate>DATE</pubDate>
+        <link>LINK</link>
+        <guid isPermaLink="true">LINK</guid>
+      </item>';
+
+my $index = "<h1>Posts</h1\n";
+my $feed = $feed_template;
+my $feed_posts = "";
+my $baseurl = "https://bckly.com";
+
+# Interpolate the current date for the feed.
+my $now = strftime("%a, %d %b %Y %H:%M:%S %z", localtime(time()));
+$feed =~ s/NOW/$now/g;
+
+sub get_title {
+    my ($page) = @_;
+    open(my $contents, $page) or die "$page: $!";
+    foreach(<$contents>) {
+        /^title: (.*)$/ && return $1;
+    }
+    return undef;
+}
+
+sub xml_for_item {
+    my ($title, $date, $href, $description) = @_;
+    
+    # XML-escape the description
+    $description =~ s/&/&amp;/g;
+    $description =~ s/</&lt;/g;
+    $description =~ s/>/&gt;/g;
+
+    my @lt = localtime(Time::Piece->strptime($date, "%Y-%M-%d"));
+    $date = strftime("%a, %d %b %Y %H:%M:%S %z", @lt[0..8]); # chokes on the last list elem for some reason
+    
+    $_ = $item_template;
+    s/TITLE/$title/g;
+    s/DATE/$date/g;
+    s/LINK/${baseurl}${href}/g;
+    s/DESCRIPTION/$description/;
+    return $_;
+}
+
+opendir(my $pages, "posts") or die "Couldn't open dir";
+my @pages = readdir($pages);
+
+for (reverse(sort(@pages))) {
+    if ($_ =~ /[0-9-]+(.*)\.markdown/) {
+        my $title = get_title("posts/$_");
+        my ($date) = /^(\d{4}-\d{2}-\d{2})/;
+        (my $href = "posts/$_") =~ s/markdown/html/;
+        my $description = `pandoc --to html5 posts/$_`;
+        $index .= "<div><span class=\"date\">$date</span> <a href=\"/$href\"><h3 class=\"post-listing\">$title</h3></a></div>\n";
+        $feed_posts .= xml_for_item($title, $date, $href, $description)
+    }
+}
+
+$feed =~ s/POSTS/$feed_posts/;
+$index .= "<footer><a href=\"about.html\">About</a> | <a href=\"/feed.xml\">Subscribe via RSS</a></footer>";
+
+closedir($pages);
+
+# Output feed or index, depending on the argument passed.
+if ($ARGV[0] eq "feed") {
+    print $feed;
+} elsif ($ARGV[0] eq "index") {
+    print $index;
+} else {
+    print "Usage: ./gen {feed, index}\n";
+    exit 1;
+}
